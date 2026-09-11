0de7dce9b6d34db8219db28add9572cad6dbc4e3
Asad <asadlangdebw@gmail.com>
2026-09-11 13:07:34 +0530

    chat_prompt_template added

---
+++ chat_prompt_template.py
@@ -0,0 +1,10 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import SystemMessage,HumanMessage
+
+chat_template = ChatPromptTemplate([
+    ('system','Your are a helpful {domain} expert'),
+    ('human','Explain in simple term what is {topic}')
+])
+
+prompt = chat_template.invoke({'domain':'cricket','topic':'dusera'})
+print(prompt)
